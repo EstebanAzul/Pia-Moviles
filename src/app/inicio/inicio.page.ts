15969373fa8f61963c1eb3b77cd3758690dc464f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 14:08:05 -0600

    Home avance

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,3 +1,4 @@
+import { ControlInfoService } from './../galeria/control/control-info.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  autos = [];
+  constructor(public auto: ControlInfoService) {
+    this.autos = auto.auto;
+   }
 
-  constructor() { }
 
   ngOnInit() {
   }
